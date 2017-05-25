7bca4bd66d5e9b6b585ae88e9508148dfb5435d9
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-05-25 10:39:57 -0500

    Call into managed_dll for test
    Obviously, this crashes at runtime:
    java.lang.UnsatisfiedLinkError: Native library
    (com/sun/jna/android-x86/libjnidispatch.so) not found in resource path
    (.)

--- app/src/main/java/com/xamarin/embeddinated/MainActivity.java
+++ app/src/main/java/com/xamarin/embeddinated/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import managed_dll.BuiltinTypes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                BuiltinTypes b = new BuiltinTypes();
+                Snackbar.make(view, "Hello from " + b.returnsString(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
