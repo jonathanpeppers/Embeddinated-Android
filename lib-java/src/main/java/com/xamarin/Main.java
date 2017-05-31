86279804e2e46ec8261c4685fbb0217b56742a16
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-05-31 16:09:34 -0500

    Latest managed.jar from E4K

--- lib-java/src/main/java/com/xamarin/Main.java
+++ lib-java/src/main/java/com/xamarin/Main.java
@@ -4,7 +4,7 @@ import managed_dll.methods.Parameters;
 
 public class Main {
     public static void main(String[] args) {
-        String text = Parameters.concat("Hello, ", "World");
+        String text = Parameters.concat("Hello, ", "Embeddinator!");
         System.out.println(text);
     }
 }
