87cf889c98221e0755427181a26b1cac697b8e17
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-05-31 11:25:53 -0500

    Imported managed.jar, example calling into it

--- lib-java/src/main/java/com/xamarin/Main.java
+++ lib-java/src/main/java/com/xamarin/Main.java
@@ -1,7 +1,10 @@
 package com.xamarin;
 
+import managed_dll.methods.Parameters;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String text = Parameters.concat("Hello, ", "World");
+        System.out.println(text);
     }
 }
